28984c75b165adba4f0d08fad78100242ff5956f
P Balaramakrishna Varma <balaramakrsihna.p@studnets.iiit.ac.in>
2023-02-18 22:51:20 +0530

    Structuring

--- generate_reports.sh
+++ generate_reports.sh
@@ -1,4 +1,4 @@
-python smoothing.py w "./Pride and Prejudice - Jane Austen.txt" 1
-python smoothing.py k "./Pride and Prejudice - Jane Austen.txt" 2
-python smoothing.py w "./Ulysses - James Joyce.txt" 3
-python smoothing.py k "./Ulysses - James Joyce.txt" 4
+python language_model.py w "./Pride and Prejudice - Jane Austen.txt" 1
+python language_model.py k "./Pride and Prejudice - Jane Austen.txt" 2
+python language_model.py w "./Ulysses - James Joyce.txt" 3
+python language_model.py k "./Ulysses - James Joyce.txt" 4
